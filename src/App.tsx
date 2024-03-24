698725f6abecfd14f04c4ba5ebc4a0abdc150b56
Rafael Pianaro <rafaelpianaro@gmail.com>
2024-03-24 11:17:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="text-red-500">
+      TailwindCss test
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,tx,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
