542f4a5c8c23f7ed10651bde19b81817520e9f64
Rafael Pianaro <rafaelpianaro@gmail.com>
2024-03-25 22:17:32 -0300

    feat: add app component & layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react"
+import { answerT } from "./types/question";
+import Layout from "./layout";
+
 function App() {
+  const [done, setDone] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState<answerT[]>([]);
 
   return (
-    <div className="text-red-500">
-      TailwindCss test
-    </div>
+    <Layout>
+      {done ? "done" : "quiz"}
+    </Layout>
   )
 }
 
---
+++ src/layout/index.tsx
@@ -0,0 +1,18 @@
+import FormHeader from "../components/FormHeader";
+
+interface LayoutProps {
+    children:React.ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center p-[15px]">
+        <div className="w-[670px] bg-white rounded-[10px] pb-5">
+            <FormHeader />
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Layout
